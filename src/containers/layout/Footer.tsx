a7ba96d04f332be3c3fac8d70c9a4a35f72c8103
Harish-b-03 <harishbalaji369@gmail.com>
2023-08-14 15:39:50 +0530

    footer updated

--- src/containers/layout/Footer.tsx
+++ src/containers/layout/Footer.tsx
@@ -1,7 +1,25 @@
+import HeartIcon from "../../components/icons/HeartIcon";
+
 const Footer = () => {
     return (
-        <div className="w-full py-10 text-center font-bold bg-[rgba(0,0,0,0.06)]">
-            All rights resevered
+        <div className="w-full flex justify-center items-center">
+            <div className="px-5 pt-4 pb-0 w-full text-sm border-t border-gray-200 text-gray-500 flex justify-center items-center">
+                <span className="flex">
+                    Made with{" "}
+                    <span className="mx-1 opacity-70">
+                        <HeartIcon />
+                    </span>
+                    by
+                    <a
+                        href="https://www.linkedin.com/in/harish-b-42a7911b9/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mx-1 underline underline-offset-2 font-semibold text-gray-600 hover:text-black"
+                    >
+                        Harish
+                    </a>
+                </span>
+            </div>
         </div>
     );
 };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,46 +4,51 @@ const about = () => {
     return (
         <Layout>
             <div className="w-full h-full min-h-[400px] flex justify-center items-center">
-                <div className="my-20 p-12 w-full text-justify max-w-2xl rounded-md border border-gray-300">
+                <div className="my-20 p-5 md:p-12 w-full text-justify max-w-2xl rounded-md border border-gray-300">
                     <div className="mb-8 text-center font-bold font-lekton text-2xl text-black">
                         About
                     </div>
                     <div className="my-2">
                         <span>
-                            <b>Store.</b> isn't a conventional ecommerce site;
-                            it's a personal project designed to showcase my web
+                            <b>Store.</b> isn&apos;t a conventional ecommerce site;
+                            it&apos;s a personal project designed to showcase my web
                             development skills. As a fellow developer, I aimed
                             to create an app that not only highlights my
                             abilities but also connects with the technical
                             aspects that inspire us.
                         </span>
                     </div>
-                    <div className="my-6 flex space-x-5">
+                    <div className="my-6 flex flex-col lg:flex-row lg:space-x-5">
                         <div className="min-w-max">
                             <b>Tech Stack:</b>
                         </div>
                         {/* NextJS, ReactJS, Typescript,
 						TailwindCSS and Fakestore API. */}
-                        <div className="flex flex-wrap">
+                        <div className="w-full lg:w-fit flex flex-wrap">
                             <img
                                 src="https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white"
                                 alt="NextJS-Badge"
+                                className="w-fit h-fit"
                             />
                             <img
                                 src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
                                 alt="ReactJS-Badge"
+                                className="w-fit"
                             />
                             <img
                                 src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white"
                                 alt="TailwindCSS-Badge"
+                                className="w-fit"
                             />
                             <img
                                 src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
                                 alt="Typescript-Badge"
+                                className="w-fit"
                             />
                             <img
                                 src="https://img.shields.io/badge/FakeStoreAPI-430098?style=for-the-badge&logo=RESTapi&logoColor=white"
                                 alt="FakeStoreAPI-Badge"
+                                className="w-fit"
                             />
                         </div>
                     </div>
